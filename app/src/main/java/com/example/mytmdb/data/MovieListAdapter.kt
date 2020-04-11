39333733a3265a633365daa77bea8ac5cf97570e
Yakov9293 <yakov226805@yandex.ru>
2020-04-11 11:04:56 +0400

    Clean code; Реализовал ListAdapter, binding. Позиция RecyclerView при этом сохраняется.

--- app/src/main/java/com/example/mytmdb/data/MovieListAdapter.kt
+++ app/src/main/java/com/example/mytmdb/data/MovieListAdapter.kt
@@ -14,22 +14,25 @@ import com.example.mytmdb.R
 import com.example.mytmdb.fragments.MoviesFragmentDirections
 import com.timqi.sectorprogressview.ColorfulRingProgressView
 
-class MovieListAdapter(val data: List<SimplifiedMovie>) :
-   /*ListAdapter<SimplifiedMovie, RecyclerView.ViewHolder>(MovieDiffCallback()) {
+class MovieListAdapter() :
+    ListAdapter<SimplifiedMovie, RecyclerView.ViewHolder>(MovieDiffCallback()) {
 
     class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MovieViewHolder(LayoutInflater.from(parent.context).inflate(
-            R.layout.movies_list_item,
-            parent,
-            false
-        ))
+        return MovieViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.movies_list_item,
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder.itemView.findViewById<TextView>(R.id.title).text = getItem(position).title
-        holder.itemView.findViewById<TextView>(R.id.original_title).text = getItem(position).original_title
+        holder.itemView.findViewById<TextView>(R.id.original_title).text =
+            getItem(position).original_title
         holder.itemView.findViewById<TextView>(R.id.ratingDigit).text =
             "${(getItem(position).vote_average * 10).toInt()}%"
         holder.itemView.findViewById<ColorfulRingProgressView>(R.id.rating).percent =
@@ -41,45 +44,9 @@ class MovieListAdapter(val data: List<SimplifiedMovie>) :
                 )
             it.findNavController().navigate(directions)
         }
-        Glide.with(holder.itemView).load("https://image.tmdb.org/t/p/w500${getItem(position).poster_path}")
+        Glide.with(holder.itemView)
+            .load("https://image.tmdb.org/t/p/w500${getItem(position).poster_path}")
             .into(holder.itemView.findViewById(R.id.poster))
-    }
-}*/
-
-RecyclerView.Adapter<MovieListAdapter.MoviesListViewHolder>() {
-    class MoviesListViewHolder(val view: View) : RecyclerView.ViewHolder(view)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesListViewHolder {
-        return MoviesListViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.movies_list_item,
-                parent,
-                false
-            )
-        )
-    }
-
-    override fun getItemCount(): Int {
-        return data.size
-    }
-
-    override fun onBindViewHolder(holder: MoviesListViewHolder, position: Int) {
-        holder.view.findViewById<TextView>(R.id.title).text = data[position].title
-        holder.view.findViewById<TextView>(R.id.original_title).text = data[position].original_title
-        holder.view.findViewById<TextView>(R.id.ratingDigit).text =
-            "${(data[position].vote_average * 10).toInt()}%"
-        holder.view.findViewById<ColorfulRingProgressView>(R.id.rating).percent =
-            (data[position].vote_average * 10).toFloat()
-        holder.view.setOnClickListener {
-            val directions =
-                MoviesFragmentDirections.actionMoviesFragmentToMovieFragment(
-                    data[position].id
-                )
-            it.findNavController().navigate(directions)
-        }
-        Glide.with(holder.view).load("https://image.tmdb.org/t/p/w500${data[position].poster_path}")
-            .into(holder.view.findViewById(R.id.poster))
-
     }
 }
 
@@ -87,13 +54,14 @@ private class MovieDiffCallback : DiffUtil.ItemCallback<SimplifiedMovie>() {
 
     override fun areItemsTheSame(oldItem: SimplifiedMovie, newItem: SimplifiedMovie): Boolean {
         val res = oldItem.id == newItem.id
-        Log.d("MovieListAdapter: items",  res.toString())
+        Log.d("MovieListAdapter: items", res.toString())
         return res
     }
 
     override fun areContentsTheSame(oldItem: SimplifiedMovie, newItem: SimplifiedMovie): Boolean {
-        val res = (oldItem.title == newItem.title && oldItem.original_title == newItem.original_title && oldItem.poster_path == newItem.poster_path && oldItem.vote_average == newItem.vote_average)
-        Log.d("MovieListAdapter: cont",  res.toString())
+        val res =
+            (oldItem.title == newItem.title && oldItem.original_title == newItem.original_title && oldItem.poster_path == newItem.poster_path && oldItem.vote_average == newItem.vote_average)
+        Log.d("MovieListAdapter: cont", res.toString())
         return res
     }
 }
--- app/src/main/java/com/example/mytmdb/fragments/MoviesFragment.kt
+++ app/src/main/java/com/example/mytmdb/fragments/MoviesFragment.kt
@@ -5,45 +5,64 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytmdb.R
 import com.example.mytmdb.data.MovieService
 import com.example.mytmdb.data.MovieListAdapter
+import com.example.mytmdb.databinding.FragmentMoviesBinding
 import kotlinx.android.synthetic.main.fragment_movies.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MoviesFragment : Fragment() {
+    private val adapter = MovieListAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_movies, container, false)
+        val binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        context ?: return binding.root
+
+        binding.moviesList.layoutManager = LinearLayoutManager(activity)
+        binding.moviesList.adapter = adapter
+
+
+        return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerMovies = (movies_list as RecyclerView)
-        recyclerMovies.layoutManager = LinearLayoutManager(activity)
+        getPopularMovies()
+    }
 
-
+    private fun getPopularMovies() {
         GlobalScope.launch(Dispatchers.Main) {
             val popularMoviesRequest = MovieService.tmdbApi.getPopular()
             try {
                 val response = popularMoviesRequest.await()
                 if (response.isSuccessful) {
                     val popularMovies = response.body()?.results
-                    popularMovies?.let { recyclerMovies.adapter = MovieListAdapter(it) }
+                    popularMovies?.let { adapter.submitList(it) }
                 } else {
-                    Log.d("MainActivity ", response.errorBody().toString())
+                    Toast.makeText(activity, response.errorBody().toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.d(TAG, response.errorBody().toString())
                 }
             } catch (e: Exception) {
-                Log.d("MainActivity ", e.toString())
+                Toast.makeText(activity, e.toString(), Toast.LENGTH_LONG).show()
+                Log.d(TAG, e.toString())
             }
         }
-
     }
+
+    companion object {
+        const val TAG = "MoviesFragment"
+    }
+
 }
