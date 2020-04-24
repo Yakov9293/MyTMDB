284edc8f7a938fb9a07bd2351f7e107dbbd87f1b
Yakov9293 <yakov226805@yandex.ru>
2020-04-24 17:03:12 +0400

    bug fix

--- app/src/main/java/com/example/mytmdb/util/BindingAdapter.kt
+++ app/src/main/java/com/example/mytmdb/util/BindingAdapter.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.mytmdb.R
 import com.example.mytmdb.util.Constants.TMDB_BASE_IMAGE_URL
 import com.timqi.sectorprogressview.ColorfulRingProgressView
 
@@ -12,8 +13,12 @@ fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         Glide.with(view.context)
             .load(TMDB_BASE_IMAGE_URL + imageUrl)
+            .error(R.drawable.placeholder_movie_poster)
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(view)
+    }
+    else{
+        view.setImageResource(R.drawable.placeholder_movie_poster)
     }
 }
 
